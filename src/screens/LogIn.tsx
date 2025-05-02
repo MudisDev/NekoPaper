2a97420483dbe80ab7ee20a4fd6fca09a8095e59
MudisDev <martin.bibiano4@outlook.com>
2025-05-02 14:24:13 -0600

    Change: Aspecto de formulario de LogIn mejorado.

--- src/screens/LogIn.tsx
+++ src/screens/LogIn.tsx
@@ -1,23 +1,54 @@
 import { useNavigation } from '@react-navigation/native'
 import React from 'react'
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, TextInput, StyleSheet } from 'react-native'
 
 export const LogIn = () => {
 
     const navigation = useNavigation();
 
 
-return (
-    <View style={{ alignItems: 'center', flex: 1 }}>
+    return (
+        <View style={{ alignItems: 'center', flex: 1, marginTop: 90 }}>
+
+            <Text style={style.title}>NekoPaper</Text>
+
+            <TextInput style={style.textinput} placeholder='Username' placeholderTextColor={"black"} />
+            <Text></Text>
+            <TextInput style={style.textinput} placeholder='Password' placeholderTextColor={"black"} />
+
+            <Text></Text>
 
 
-        <Text style={{ justifyContent: 'center' }}>LogIn Screen</Text>
-        <Text style={{ justifyContent: 'center' }}>LogIn Screen</Text>
-
-
-        <TouchableOpacity onPress={() => navigation.navigate('BottomTabNavigator') }><Text>Home</Text></TouchableOpacity>
-        <Text></Text>
-        <TouchableOpacity onPress={() => navigation.navigate('Register') }><Text>Registro</Text></TouchableOpacity>
-    </View>
-)
+            <TouchableOpacity style={style.button} onPress={() => navigation.navigate('BottomTabNavigator')}><Text style={style.textButton}>Home</Text></TouchableOpacity>
+            <Text></Text>
+            <TouchableOpacity style={style.button} onPress={() => navigation.navigate('Register')}><Text style={style.textButton}>Registro</Text></TouchableOpacity>
+        </View>
+    )
 }
+
+const style = StyleSheet.create({
+    textinput: {
+        backgroundColor: "white",
+        width: "80%",
+        height: 40,
+        borderRadius: 5,
+    },
+    button: {
+        width: "50%",
+        height: 30,
+        backgroundColor: "white",
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 20,
+    },
+    textButton: {
+        color: "black",
+        fontSize: 15,
+        textTransform: 'uppercase',
+    },
+    title: {
+        fontSize: 30,
+        marginBottom: 30,
+        textDecorationStyle: 'solid',
+    }
+})
