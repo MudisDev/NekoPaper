e06c64d4ae3547f840d60f64b2a7206c90c6aa84
MudisDev <martin.bibiano4@outlook.com>
2025-05-04 11:46:52 -0600

    Add: Clase Lista y endpoint mostrar_imagenes y mostrar_etiquetas.
    * Se movio el endpoint mostrar_imagenes de la clase Imagen a la clase Lista.
    * La clase Lista gestiona la consulta de las imagenes y etiquetas a la BD.
    * Los endpoints devuelven de momento todas las imagenes y etiquetas.

--- src/php/api/imagen/mostrar_imagenes.php
+++
@@ -1,8 +0,0 @@
-<?php
- require_once '../../clases/Conexion.php';
-
- $conexion = new Conexion();
- $resultado = $conexion->SetSelect("Imagen");
- echo json_encode($resultado);
-
-?>
---
+++ src/php/clases/Lista.php
@@ -0,0 +1,37 @@
+<?php
+require_once 'Conexion.php';
+
+class Lista
+{
+    private $lista = [];
+
+    public function __construct()
+    {
+
+    }
+
+    public function Consultar_Imagenes()
+    {
+        $conexion = new Conexion();
+        $resultado = $conexion->SetSelect("Imagen");
+        $this->Set_Lista($resultado);
+    }
+
+    public function Consultar_Etiquetas()
+    {
+        $conexion = new Conexion();
+        $resultado = $conexion->SetSelect("Etiqueta");
+        $this->Set_Lista($resultado);
+    }
+
+    public function Set_Lista($datos)
+    {
+        $this->lista = $datos;
+    }
+
+    public function Get_Lista(){
+        return $this->lista;
+    }
+}
+
+?>
