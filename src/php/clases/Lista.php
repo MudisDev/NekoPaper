4d212634b273c7a53fca7fc0a14075df9cfa4ada
MudisDev <martin.bibiano4@outlook.com>
2025-05-07 15:33:08 -0600

    Add: Filtrado de imagenes.
    * Se ha implementado una filtracion de imagenes, donde solo se muestre imagenes con clasificacion 'safe'

--- src/php/clases/Lista.php
+++ src/php/clases/Lista.php
@@ -12,14 +12,15 @@ class Lista
 
     public function Consultar_Imagenes()
     {
+        $condiciones = "clasificacion = 'safe'";
         $conexion = new Conexion();
-        $resultado = $conexion->SetSelect("Imagen");
+        $resultado = $conexion->SetSelect("Imagen", ["*"], $condiciones);
         $this->Set_Lista($resultado);
     }
 
     public function Consultar_Imagenes_Favoritas($id_usuario)
     {
-        $condicion = "id_usuario = '$id_usuario'";
+        $condicion = "id_usuario = '$id_usuario' AND clasificacion = 'safe'";
         $conexion = new Conexion();
         $resultado = $conexion->SetSelect("Vista_Favorito", ["*"], $condicion);
         $this->Set_Lista($resultado);
@@ -27,7 +28,7 @@ class Lista
 
     public function Consultar_Imagenes_Por_Etiqueta($id_etiqueta)
     {
-        $condicion = "id_etiqueta = '$id_etiqueta'";
+        $condicion = "id_etiqueta = '$id_etiqueta' AND clasificacion = 'safe'";
         $conexion = new Conexion();
         $resultado = $conexion->SetSelect("Vista_Mostrar_Imagen_Por_Etiqueta", ["*"], $condicion);
         $this->Set_Lista($resultado);
@@ -35,8 +36,9 @@ class Lista
 
     public function Consultar_Etiquetas()
     {
+        $condiciones = "lista_negra = 0";
         $conexion = new Conexion();
-        $resultado = $conexion->SetSelect("Etiqueta");
+        $resultado = $conexion->SetSelect("Etiqueta", ["*"], $condiciones);
         $this->Set_Lista($resultado);
     }
 
